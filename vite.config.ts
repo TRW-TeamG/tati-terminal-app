f31ef6dfc319add3dae2bb6ff524c69a3f7bef41
Andre <10810328+myx0m0p@users.noreply.github.com>
2025-01-17 00:33:16 +0700

    add github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/tati-fortune-teller/', // Replace with your repo name
 });
